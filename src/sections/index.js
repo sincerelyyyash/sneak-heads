3e45e0827f0521dcbf8dedd0068c5e06e3c2c8a6
sincerelyyyash <yta0088@gmail.com>
2024-01-02 02:27:11 +0530

    Initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+import Services from "./Services";
+import Subscribe  from "./Subscribe";
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    CustomerReviews,
+    Footer,
+    SpecialOffer,
+    Services,
+    Subscribe,
+}
