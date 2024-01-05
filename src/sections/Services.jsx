e2de00f690d5628c7898faf102dada6dbc724eda
sincerelyyyash <yta0088@gmail.com>
2024-01-05 21:31:17 +0530

    Special Offer, Services component added
    Specail Offer, Service Card, Services, Super Quality component updated and added

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,8 +1,16 @@
-
+import ServiceCard from "../Components/ServiceCard"
+import { services } from "../Constants"
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="max-container flex justify-center flex-wrap
+    gap-9">
+      {services.map((service)=> (
+        <ServiceCard key={service.label}
+        {...service}/>
+      ))}
+
+    </section>
   )
 }
 
