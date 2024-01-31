d91ee6e3e61521a9bd39f6142548d73514e3a11f
sincerelyyyash <yta0088@gmail.com>
2024-02-01 03:10:51 +0530

    Login, logout routes and controllers, authMiddleware added

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -10,10 +10,30 @@ const registerBody = zod.object({
     password: zod.string()
 })
 
+const loginBody = zod.object({
+    email: zod.string().email(),
+    password: zod.string()
+})
+
+const generateAccessAndRefreshToken = async (userId)=>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+
+        return {accessToken, refreshToken}
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong")
+    }
+}
+
 const registerUser = asyncHandler(async (req, res)=>{
     const {fullname, email, password} = req.body
 
-    const { success } = signUpBody.safeParse(req.body)
+    const { success } = registerBody.safeParse(req.body)
     if(!success){
         return res.status(411).json({
             message: "Invalid Inputs"
@@ -27,7 +47,7 @@ const registerUser = asyncHandler(async (req, res)=>{
         throw new ApiError(400,"All fields are required")
     }
 
-    const existingUser = User.findOne({email})
+    const existingUser = await User.findOne({email})
     if(existingUser){
         throw new ApiError(409, "User Already exists")
     }
@@ -47,8 +67,78 @@ const registerUser = asyncHandler(async (req, res)=>{
 
 
     return res.status(201).json(
-        new ApiResponse(200, createdUser, "User Registered Successfully")
+        new ApiResponse(200, "User Registered Successfully")
     )
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async (req,res)=>{
+    const {email,password} = req.body;
+
+    const { success } = loginBodyBody.safeParse(req.body)
+    if(!success){
+        return res.status(411).json({
+            message: "Invalid Inputs"
+        })
+    }
+
+    if(!email || password){
+        throw new ApiError(400,"Email and password is required")
+    }
+
+    const user = await User.findOne(email)
+
+    if(!user){
+        throw new ApiError(404, "User does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(401, "Password Incorrect")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password - refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+    return res.status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse( 200, {
+            user: loggedInUser, accessToken, refreshToken
+        }, "User logged in succesfully")
+    )
+})
+
+const logoutUser = asyncHandler(async(req, res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined 
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out"))
+})
+
+
+
+export {registerUser, loginUser, logoutUser}
