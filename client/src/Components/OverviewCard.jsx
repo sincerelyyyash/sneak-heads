7e485eb28ee376bc038cc037c21c281336f3ce59
sincerelyyyash <yta0088@gmail.com>
2024-02-20 02:07:54 +0530

    Dashboard Overview Card changes

--- client/src/Components/OverviewCard.jsx
+++ client/src/Components/OverviewCard.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import {HiTrendingUp, HiTrendingDown} from 'react-icons/hi'
 
-function OverviewCard({ title, value, percentage }) {
+function OverviewCard({ title,amount, value, percentage }) {
   return (
       <div className='mt-5 px-5 bg-white w-72 h-28 rounded-lg shadow-3xl 
       flex items-center justify-between'>
         <div className='p-4'>
           <h4 className='text-black text-lg italic'>{title}</h4>
-          <h1 className='text-coral-red text-2xl  font-bold '>{value}</h1>
+          <h1 className='text-coral-red text-2xl  font-bold '>{amount ? `$${value}` :value }</h1>
         </div>
         <div className='relative mr-4'>
           <div className='absolute top-1/2 left-full transform -translate-y-1/2 
           -translate-x-full 
           w-16 h-16 flex justify-center items-center rounded-full 
           border-4 border-green-400 '>
-            <h3 className='text-green-600 font-xl'>{percentage}</h3>
-          </div>
+          {
+            percentage>0? (<span className='text-green'><HiTrendingUp/> +{percentage}%{" "} </span>)
+            :(<span className='text-red'><HiTrendingDown/> {percentage}%{" "} </span>)
+          }
+               </div>
         </div>
       </div>
   );
--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import SideBar from '../Components/SideBar'
 import Overview from './Overview'
+import OverviewCard from '../Components/OverviewCard'
 
 function Dashboard() {
   return (
     <div className='grid grid-cols-2 gap-8 h-screen bg-gray-100'>
         <SideBar/>
-        <main>Dashboard</main>
+        <main>
+     
+          <div className='flex items-center justify-evenly'>
+            <OverviewCard title={"Revenue"} value={"₹245600"} percentage={-76}/>
+          </div>
+    
+        </main>
     </div>
   )
 }
