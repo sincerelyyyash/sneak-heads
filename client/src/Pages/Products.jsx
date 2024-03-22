8be30aee97e7bde6fa38c1fb2b588183228544db
sincerelyyyash <yta0088@gmail.com>
2024-03-22 18:38:50 +0530

    Admin Product page changes

--- client/src/Pages/Products.jsx
+++ client/src/Pages/Products.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import SideBar from '../Components/SideBar'
+import PopularProductCard from '../Components/PopularProductCard'
+import { products } from "../Constants"
 
 const Products = () => {
   return (
-    <div className='grid grid-cols-2 gap-8 h-screen bg-gray-100'>
-        <SideBar/>
-        <main>Products</main>
+    <div className='flex flex-row gap-8 h-screen bg-gray-100'>
+        <SideBar className='basis-1/4'/>
+        <main>
+        <div className='flex flex-row justify-between'>
+        <div
+        className="mt-16 grid lg:grid-cols-3 md:grid-cols-3 
+        sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14">
+          {products.map((product)=>(
+            <PopularProductCard key=
+            {product.name} {...product}/>
+          ))}
+        </div>
+        </div>
+        </main>
     </div>
   )
 }
