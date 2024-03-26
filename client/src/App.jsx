449eb643e385fa869f799d1ef08ccc2b64e1b10c
sincerelyyyash <yta0088@gmail.com>
2024-03-27 02:02:30 +0530

    Admin components & pages removed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,14 +10,7 @@ import Loader from "./Components/Loader";
 const Signin = lazy(()=>import("./Pages/Signin"));
 const Homepage = lazy(()=> import("./Pages/Homepage"));
 const Signup = lazy(()=> import("./Pages/Signup"));
-const Overview = lazy(()=>import("./Pages/Overview"));
-const Dashboard = lazy(()=>import( "./Pages/Dashboard"));
-const Products  = lazy(()=>import("./Pages/Products"));
-const Customer  = lazy(()=>import("./Pages/Customer"));
-const Transaction  = lazy(()=>import("./Pages/Transaction"));
-const BarChart = lazy(()=>import("./Pages/BarChart"))
-const LineChart = lazy(()=>import("./Pages/LineChart"))
-const PieChart = lazy(()=>import("./Pages/PieChart"))
+
 
 
 const App = () => (
@@ -28,18 +21,8 @@ const App = () => (
     <Route path="/" element={<Homepage/>}/>
     <Route path="/signin" element={<Signin/>}/>
     <Route path="/signup" element={<Signup/>}/>
-
-    <Route path="/admin/dashboard" element={<Dashboard/>}/>
-    <Route path="/admin/products" element={<Products/>}/>
-    <Route path="/admin/customer" element={<Customer/>}/>
-    <Route path="/admin/transaction" element={<Transaction/>}/>
-
-    <Route path="/admin/bar" element={<BarChart/>}/>
-    <Route path="/admin/pie" element={<LineChart/>}/>
-    <Route path="/admin/line" element={<PieChart/>}/>
   </Routes>
   </Suspense>
-  
   </BrowserRouter>
   </RecoilRoot>
 )
