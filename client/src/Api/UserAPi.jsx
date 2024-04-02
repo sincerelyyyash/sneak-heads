81f2f08f4a19f9110aefd7345d14c65f2925eb23
sincerelyyyash <yta0088@gmail.com>
2024-04-03 03:17:19 +0530

    User Api functions added

--- client/src/Api/UserAPi.jsx
+++ client/src/Api/UserAPi.jsx
@@ -1,10 +1,12 @@
 import axios from "axios"
 import { baseUrl } from "../Constants";
 
-
-
-
-
+const token = localStorage.getItem("accessToken")
+const config = {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  };
 
 export const SignUp = async (email,fullname,password) => {
     try {
@@ -13,32 +15,75 @@ export const SignUp = async (email,fullname,password) => {
             fullname,
             password
         };
-
-        const response = await axios.post(baseUrl+'/users/register', requestBody)
-
+        const response = await axios.post(baseUrl+'/users/register', requestBody, {
+            withCredentials: true,
+        })
         localStorage.setItem("accessToken", response.data.accessToken);
-        // setAccessToken(response.data.accessToken);
 
     } catch (response) {
-        console.log('Error signing up:', response);
+        throw error;
     }
 }
 
 
 export const SignIn = async (email, password) => {
-    
     try {
         const requestBody = {
             email,
             password
         };
 
-        const response = await axios.post(baseUrl+'/users/register', requestBody)
+        const response = await axios.post(baseUrl + '/users/login', requestBody, {
+            withCredentials: true,
+        });
         localStorage.setItem("accessToken", response.data.accessToken);
-        // setAccessToken(response.data.accessToken);
-        
 
-    } catch (response) {
-        console.log(response);  
+    } catch (error) {
+        throw error;
+    }
+};
+
+
+export const Logout = async()=>{
+    try {
+        const response = await axios.post(baseUrl + '/users/logout',config)
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const changePassword = async(oldPassword, newPassword )=>{
+    try {
+        const response = await axios.post(baseUrl + '/users/change-password', {
+            oldPassword,
+            newPassword
+        },
+        {
+            withCredentials: true,
+        },config)
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const changeEmail = async(email)=>{
+    try {
+        const response = await axios.post(baseUrl +'/users/change-email',{
+            email
+        }, {
+            withCredentials: true,
+        },config)
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const getUserDetails = async()=>{
+    try {
+        const user = await axios.get(baseUrl + '/users/user', {
+            withCredentials: true,
+        },config)
+    } catch (error) {
+        throw error;
     }
 }
