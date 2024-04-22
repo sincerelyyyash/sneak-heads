95bff7578f76f56fba4c6e81f6a51fd37d7db5cc
sincerelyyyash <yta0088@gmail.com>
2024-04-22 18:23:49 +0530

    server side change for login state

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -8,11 +8,11 @@ router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJWT, logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
-router.route("/change-password").post(changePassword)
-router.route("/change-email").post(updateProfile)
+router.route("/refresh-token").post(verifyJWT, refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changePassword)
+router.route("/change-email").post(verifyJWT, updateProfile)
 
-router.route("/user").get(currentUser)
+router.route("/user").get(verifyJWT, currentUser)
 
 
 export default router
