c7979a204adc1328a0b3a5e5b8e9da551e4db536
sincerelyyyash <yta0088@gmail.com>
2024-04-29 01:36:46 +0530

    Footer updated

--- client/src/Constants/index.js
+++ client/src/Constants/index.js
@@ -3,12 +3,6 @@ import { bigShoe1, bigShoe2, bigShoe3, customer1, customer2, thumbnailShoe1, thu
 
 
 export const baseUrl = 'http://localhost:8000/api/v1'
-export const navLinks = [
-    { href: "#home", label: "Home" },
-    // { href: "#about-us", label: "About Us" },
-    { href: "#products", label: "Products" },
-    // { href: "#contact-us", label: "Contact Us" },
-];
 
 export const shoes = [
     {
@@ -53,13 +47,13 @@ export const services = [
 export const reviews = [
     {
         imgURL: customer1, 
-        customerName: 'Morich Brown',
+        customerName: 'John Brown',
         rating: 4.5,
         feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
     },
     {
         imgURL: customer2, 
-        customerName: 'Lota Mongeskar',
+        customerName: 'Jennifer',
         rating: 4.5,
         feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
     }
@@ -70,12 +64,12 @@ export const footerLinks = [
     {
         title: "Products",
         links: [
-            { name: "Air Force 1", link: "/" },
-            { name: "Air Max 1", link: "/" },
-            { name: "Air Jordan 1", link: "/" },
-            { name: "Air Force 2", link: "/" },
-            { name: "Nike Waffle Racer", link: "/" },
-            { name: "Nike Cortez", link: "/" },
+            { name: "NIKE Mens Court Royale 2", link: "/products/66072b9c2514675e27d82245" },
+            { name: "Nike Dunk Low Retro Premium", link: "/products/66072c302514675e27d82249" },
+            { name: "Nike Dunk Low Retro", link: "/products/66072cf52514675e27d8224d" },
+            { name: "Nike Air Dunk Jumbo", link: "/products/66072d922514675e27d82251" },
+            { name: "Nike Dunk High Retro SE1", link: "/products/66072e222514675e27d82255" },
+            { name: "Nike Dunk Low", link: "/products/66072f852514675e27d82261" },
         ],
     },
     {
@@ -92,14 +86,14 @@ export const footerLinks = [
         title: "Get in touch",
         links: [
             { name: "customer@sneakhead.com", link: "mailto:customer@sneakhead.com" },
-            { name: "+92554862354", link: "tel:+92554862354" },
+            { name: "+91-29291919191", link: "tel:+92111111111" },
         ],
     },
 ];
 
 export const socialMedia = [
-    { src: facebook, alt: "facebook logo" },
-    { src: twitter, alt: "twitter logo" },
-    { src: instagram, alt: "instagram logo" },
+    { src: facebook, alt: "facebook logo", link: 'https://twitter.com/sincerelyyyash' },
+    { src: twitter, alt: "twitter logo", link: 'https://twitter.com/sincerelyyyash' },
+    { src: instagram, alt: "instagram logo", link: 'https://twitter.com/sincerelyyyash' },
 ];
 export default notFoundImage;
--- client/src/sections/Footer.jsx
+++ client/src/sections/Footer.jsx
@@ -1,24 +1,22 @@
 import { footerLinks, socialMedia } from "../Constants"
 import { copyrightSign } from "../assets/icons"
 
-
 const Footer = () => {
   return (
     <footer className='max-container'>
       <div className='flex justify-between items-start gap-20 flex-wrap max-lg:flex-col'>
         <div className='flex flex-col items-start'>
           <p className='mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm'>
-            Get shoes ready for the new term at your nearest SneakHeads store. Find
-            Your perfect Size In Store. Get Rewards
+            Get shoes ready for the new term at SneakHeads. Find
+            Your perfect Sneakers. Get Rewards
           </p>
           <div className='flex items-center gap-5 mt-8'>
             {socialMedia.map((icon) => (
-              <div
-                className='flex justify-center items-center w-12 h-12 bg-white rounded-full'
-                key={icon.alt}
-              >
-                <img src={icon.src} alt={icon.alt} width={24} height={24} />
-              </div>
+              <a href={icon.link} target="_blank" rel="noopener noreferrer" key={icon.alt}>
+                <div className='flex justify-center items-center w-12 h-12 bg-white rounded-full'>
+                  <img src={icon.src} alt={icon.alt} width={24} height={24} />
+                </div>
+              </a>
             ))}
           </div>
         </div>
