6d184be6b86f7737e9ee7bab4026f54e5e667b8b
sincerelyyyash <yta0088@gmail.com>
2024-04-29 02:30:18 +0530

    Cors updated in server

--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,7 @@ app.use(cors({
 
 //Policy for cors for developement period only
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', process.env.CORS_ORIGIN,);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
